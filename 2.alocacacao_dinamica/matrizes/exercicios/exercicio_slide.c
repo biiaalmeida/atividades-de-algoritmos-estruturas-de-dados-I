b97522d739731460a8d2ab96278f07fc14e1019e
Ana Beatriz Almeida <abeatrizpb@gmail.com>
2023-12-30 13:44:00 -0300

    subindo o exercicio do slide (matrizes)

--- 2.alocacacao_dinamica/matrizes/exercicios/exercicio_slide.c
+++ 2.alocacacao_dinamica/matrizes/exercicios/exercicio_slide.c
@@ -38,39 +38,3 @@ int main() {
 
     return 0;
 }
-  
-    /*definindo a quantidade de linhas e colunas e declarando as variaveis
-    int linhas=3, colunas=3, i, j;
-
-    // Alocando vetor de ponteiros
-    int **matriz = (int **) malloc(linhas * sizeof(int *));
-
-    // Alocando cada linha da matriz
-    for (i = 0; i < linhas; i++) {
-        matriz[i] = (int *) malloc(colunas * sizeof(int));
-    }
-
-    // Preenchendo a matriz com os índices
-    for (int i = 0; i < linhas; i++) {
-        for (int j = 0; j < colunas; j++) {
-            matriz[i][j] = i * 5 + j;
-        }
-    }
-
-    // Imprimindo a matriz
-    printf("Matriz:\n");
-    for (int i = 0; i < linhas; i++) {
-        for (int j = 0; j < colunas; j++) {
-            printf("%2d ", matriz[i][j]);
-        }
-        printf("\n");
-    }
-
-    // Liberando a memória alocada
-    for (int i = 0; i < 5; i++) {
-        free(matriz[i]);
-    }
-    free(matriz);
-
-    return 0;
-}*/
--- 2.alocacacao_dinamica/matrizes/exercicios/exercicio_slide.md
+++ 2.alocacacao_dinamica/matrizes/exercicios/exercicio_slide.md
@@ -5,3 +5,5 @@ elemento da matriz com o seu respectivo índice (matriz[i][j] = i*5 + j). Em
 seguida, imprima a matriz.
 
 ###### *Resposta:* 
+O código está em outro arquivo nessa pasta, mas aqui está a foto da resolução:
+![Alt text](image.png)
