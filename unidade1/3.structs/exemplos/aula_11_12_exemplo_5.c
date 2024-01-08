07126b853e2aa501f41c88b933c3b10797b18057
Ana Beatriz Almeida <abeatrizpb@gmail.com>
2024-01-08 18:25:26 -0300

    organizing the examples

--- unidade1/3.structs/exemplos/aula_11_12_exemplo_5.c
+++ unidade1/3.structs/exemplos/aula_11_12_exemplo_5.c
@@ -1,1 +1,40 @@
 //fazer o mesmo que o exemplo 3 para funcionários públicos
+
+#include <stdio.h>
+#include <stdlib.h>
+
+struct funcionario {
+    char nome[20];
+    char cargo[20];
+    int rg;
+};
+
+void preenche(struct funcionario *servidor){
+    printf("Funcionários públicos\n");
+    printf("Digite o nome: ");
+    scanf(" %[^\n]", servidor->nome);
+    printf("Digite o cargo: ");
+    scanf(" %[^\n]", servidor->cargo);
+    printf("Digite o RG: ");
+    scanf("%i", &servidor->rg);
+}
+
+void imprime(struct funcionario *servidor){
+    printf("Nome: %s\n", servidor->nome);
+    printf("Cargo: %s\n", servidor->cargo);
+    printf("RG: %i\n", servidor->rg);
+}
+
+int main(){
+    struct funcionario *servidor = (struct funcionario*) malloc(sizeof(struct funcionario));
+
+    if(servidor == NULL){
+        exit(1);
+    }
+
+    preenche(servidor);
+    imprime(servidor);
+
+    free(servidor);
+    return 0;
+}
