129bcee4b5404aa14b2befbd06ec490497f5fa06
Ana Beatriz Almeida <abeatrizpb@gmail.com>
2024-01-16 10:37:28 -0300

    implementing the function pessoaMaisVelha

--- unidade1/3.structs/lista_stucts/002_resposta.c
+++ unidade1/3.structs/lista_stucts/002_resposta.c
@@ -4,7 +4,7 @@ a) Escreva uma função que receba como parâmetro o endereçoo de uma estrutura
 preencha seus campos com valores fornecidos pelo usuário via teclado. OK
 b) Escreva uma função que receba como parâmetro o endereço de uma estrutura do tipo Pessoa e
 imprima os valores dos seus campos.   OK
-c) Implemente uma função para realizar uma atualização da idade de uma estrutura do tipo Pessoa.   
+c) Implemente uma função para realizar uma atualização da idade de uma estrutura do tipo Pessoa.  OK 
 d) Escreva uma função que receba como parâmetro um vetor de estruturas do tipo Pessoa e imprima
 o nome da Pessoa mais velha e mais nova */
 
@@ -41,8 +41,22 @@ void imprima (Pessoa * individuo){
     printf("Idade: %d\n", individuo->idade);
 }
 
+/*função que cumpre com o intem C solicitado:  Implemente uma função para realizar uma atualização da idade de uma estrutura do tipo Pessoa.*/
 void atualizaIdade(Pessoa * individuo, int nova_idade){
     individuo->idade = nova_idade;
+}
+
+void pessoaMaisVelha(Pessoa * individuo, int quantidadePessoas){
+    int contador;
+    int maior_idade = individuo[0].idade;
+    int id = 0;
+    for (contador = 0; contador < quantidadePessoas; contador++){
+        if (individuo[contador].idade > maior_idade){
+           maior_idade = individuo[contador].idade;
+           id = contador;
+        }
+    }
+    printf("\n A pessoa mais velha é %s que tem %d anos", individuo[id].nome, maior_idade);
 }
 
 int main(){
