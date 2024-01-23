240e8ff708e9ec1ebbbf2e6b4c64eea31c02fb31
Ana Beatriz Almeida <112524121+biiaalmeida@users.noreply.github.com>
2024-01-23 17:28:13 -0300

    Update 001_questao.c

--- unidade1/4.enum e union/enum/lista/001_questao.c
+++ unidade1/4.enum e union/enum/lista/001_questao.c
@@ -1,27 +1,34 @@
 #include <stdio.h>
 
-typedef enum genero{
-    masculino, 
+typedef enum genero {
+    masculino,
     feminino
-}Genero;
+} Genero;
 
-typedef struct pessoa{
+typedef struct pessoa {
     char nome[20];
     int idade;
-    Genero genero_pessoa
-}Pessoa;
+    Genero genero_pessoa;
+} Pessoa;
 
-void get_dados(Pessoa * pessoa){
+void get_dados(Pessoa *pessoa) {
     printf("Informe o nome: \n");
     scanf(" %[^\n]", pessoa->nome);
     printf("Informe a idade: \n");
     scanf("%d", &pessoa->idade);
-    printf("Informe o genero: \n 0-masculino \n 1- feminino");
-    scanf("%d", &pessoa->genero_pessoa);
+    printf("Informe o genero (0-masculino, 1-feminino): \n");
+    scanf("%d", (int *)&pessoa->genero_pessoa);
 }
 
-void imprime_pessoa(Pessoa * pessoa){
+void imprime_pessoa(Pessoa *pessoa) {
     printf("Nome: %s\n", pessoa->nome);
     printf("Idade: %d\n", pessoa->idade);
-    printf("Gênero: %s", pessoa->genero_pessoa);
+    printf("Gênero: %s\n", pessoa->genero_pessoa == masculino ? "Masculino" : "Feminino");
+}
+
+int main() {
+    Pessoa pessoa;
+    get_dados(&pessoa); 
+    imprime_pessoa(&pessoa);
+    return 0;
 }
