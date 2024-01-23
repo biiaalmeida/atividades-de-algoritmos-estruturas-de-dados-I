3df16e80ddb94e3ec558d1f2c03f0efc8fd357b6
Ana Beatriz Almeida <abeatrizpb@gmail.com>
2024-01-23 13:58:25 -0300

    questao um

---
+++ lista_tipos_estruturados/001_questao.md
@@ -0,0 +1,19 @@
+### Questão 1
+---
+Crie um tipo estruturado para armazenar dados de um ingresso. Uma estrutura deste tipo possui os
+seguintes campos: preço, local e atração.   
+a) Escreva uma função que receba como parâmetro o endereço de uma estrutura do tipo Ingresso e
+preencha seus campos com valores fornecidos pelo usuário via teclado. Protótipo: void
+preenche(Ingresso* i);   
+b) Escreva uma função que receba como parâmetro o endereço de uma estrutura do tipo Ingresso e imprima
+os valores dos seus campos. Protótipo: void imprime(Ingresso* i);   
+c) Escreva uma função que receba, como parâmetros, o endereço de uma estrutura do tipo Ingresso e um
+novo valor de preço e atualize o preço do ingresso para o novo valor. Protótipo: void
+altera_preco(Ingresso* i, float valor);   
+d) Escreva uma função que receba, como parâmetros, um vetor de ingressos e o tamanho do vetor e
+imprima os eventos de ingresso mais barato e mais caro. Protótipo: void imprime_menor_maior_preco(int
+n, Ingresso* vet);   
+e) Escreva uma função main para testar as funções anteriores.   
+
+###### *Resposta:* 
+O código está em outro arquivo nessa pasta, mas aqui está a foto da resolução:
---
+++ lista_tipos_estruturados/001_respostas.c
@@ -0,0 +1,54 @@
+/*Crie um tipo estruturado para armazenar dados de um ingresso. Uma estrutura deste tipo possui os
+seguintes campos: preço, local e atração.   OK
+a) Escreva uma função que receba como parâmetro o endereço de uma estrutura do tipo Ingresso e
+preencha seus campos com valores fornecidos pelo usuário via teclado. Protótipo: void
+preenche(Ingresso* i);   OK
+b) Escreva uma função que receba como parâmetro o endereço de uma estrutura do tipo Ingresso e imprima
+os valores dos seus campos. Protótipo: void imprime(Ingresso* i);   OK
+c) Escreva uma função que receba, como parâmetros, o endereço de uma estrutura do tipo Ingresso e um
+novo valor de preço e atualize o preço do ingresso para o novo valor. Protótipo: void
+altera_preco(Ingresso* i, float valor);   
+d) Escreva uma função que receba, como parâmetros, um vetor de ingressos e o tamanho do vetor e
+imprima os eventos de ingresso mais barato e mais caro. Protótipo: void imprime_menor_maior_preco(int
+n, Ingresso* vet);   
+e) Escreva uma função main para testar as funções anteriores.*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+void divisoria(){
+    printf("\n--------------------------------------------------------------\n");
+}
+
+typedef struct ingresso{
+    float preco;
+    char local[20];
+    char atracao[20];
+}Ingresso;
+
+void preenche(Ingresso * i){
+    printf("Preço: ");
+    scanf("%f", &i->preco);
+    printf("Local: ");
+    scanf(" %[^\n]", i->local);
+    printf("Atração: ");
+    scanf(" %[^\n]", i->atracao);
+}
+
+void imprimir(Ingresso * i){
+    printf("Preço: %.2f\n", i->preco);
+    printf("Local: %s\n", i->local);
+    printf("Atração: %s\n", i->atracao);
+}
+
+int main(){
+    Ingresso * i = malloc(sizeof(Ingresso));
+    printf("Forneça os dados do seu ingresso:");
+    divisoria();
+    preenche(i);
+    printf("Dados do ingresso fornecidos");
+    divisoria();
+    imprimir(i);
+    free(i);
+    return 0;
+}
