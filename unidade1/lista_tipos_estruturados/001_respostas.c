e4a48915b6c9d8129fcc9d77be735ac997881a0a
Ana Beatriz Almeida <112524121+biiaalmeida@users.noreply.github.com>
2024-01-27 11:07:59 -0300

    adding the first question

--- unidade1/lista_tipos_estruturados/001_respostas.c
+++ unidade1/lista_tipos_estruturados/001_respostas.c
@@ -7,18 +7,14 @@ b) Escreva uma função que receba como parâmetro o endereço de uma estrutura 
 os valores dos seus campos. Protótipo: void imprime(Ingresso* i);   OK
 c) Escreva uma função que receba, como parâmetros, o endereço de uma estrutura do tipo Ingresso e um
 novo valor de preço e atualize o preço do ingresso para o novo valor. Protótipo: void
-altera_preco(Ingresso* i, float valor);   
+altera_preco(Ingresso* i, float valor);  OK 
 d) Escreva uma função que receba, como parâmetros, um vetor de ingressos e o tamanho do vetor e
 imprima os eventos de ingresso mais barato e mais caro. Protótipo: void imprime_menor_maior_preco(int
 n, Ingresso* vet);   
-e) Escreva uma função main para testar as funções anteriores.*/
+e) Escreva uma função main para testar as funções anteriores.   OK*/ 
 
 #include <stdio.h>
 #include <stdlib.h>
-
-void divisoria(){
-    printf("\n--------------------------------------------------------------\n");
-}
 
 typedef struct ingresso{
     float preco;
@@ -27,6 +23,7 @@ typedef struct ingresso{
 }Ingresso;
 
 void preenche(Ingresso * i){
+    printf("Forneça os dados do seu ingresso\n");
     printf("Preço: ");
     scanf("%f", &i->preco);
     printf("Local: ");
@@ -35,34 +32,91 @@ void preenche(Ingresso * i){
     scanf(" %[^\n]", i->atracao);
 }
 
-void imprimir(Ingresso * i){
+void imprime(Ingresso * i, int n){
+    printf("Dados do ingresso nº %d\n", n+1);
     printf("Preço: %.2f\n", i->preco);
     printf("Local: %s\n", i->local);
     printf("Atração: %s\n", i->atracao);
 }
 
-void altera_preco (Ingressso *i, float valor){
+void altera_preco (Ingresso *i, float valor){
+    i->preco = valor;
+    printf("O valor agora é: %.2f \n", i->preco);
+}
 
+void imprime_maior_e_menor(int n, Ingresso * vet){
+    int contador;
+    float maior = 0;
+    float menor = vet[0].preco;
+
+    for (contador = 0; contador < n; contador++){
+        if (vet[contador].preco > maior){
+            maior = vet[contador].preco;
+        }
+        if (vet[contador].preco < menor){
+            menor = vet[contador].preco;
+        }
+    }
+    
+    printf("\nO menor valor é %.2f e o maior é %.2f\n", menor, maior );
 }
 
 int main(){
+    Ingresso ingresso;
+    
+    int quant;
+    int cont;
     int op;
     float valor;
-    Ingresso * i = malloc(sizeof(Ingresso));
-    printf("Forneça os dados do seu ingresso:");
-    divisoria();
-    preenche(i);
-    printf("Dados do ingresso fornecidos");
-    divisoria();
-    imprimir(i);
-    free(i);
-    printf("Deseja modificar o valor do ingresso? \n 1-sim \n 2-não");
-    scanf("%d", &op);
-    
-    if(op = 1){
-        printf("alterou");
-    } else{
-        printf("tenha um bom show");
+
+    printf("Quantos ingressos serão cadastrados?");
+    scanf("%d", &quant);
+
+    Ingresso vetor_i[quant];
+
+     for (cont = 0; cont < quant; cont++){
+        preenche(&ingresso);
+        vetor_i[cont] = ingresso; 
+    }
+
+    for (cont = 0; cont < quant; cont++){
+        imprime(&vetor_i[cont], cont);
     }
     
-    retu
+    imprime_maior_e_menor(quant, vetor_i);
+
+    //alterando os valores dos ingressos
+    printf("Deseja modificar o valor do ingresso? \n 1-sim \n 2-não \n");
+    scanf("%d", &op);
+    
+    switch (op){
+    case 1:
+        printf("Digite o id do ingresso a ser alterado: \n");
+        scanf("%d", &op);
+
+        if (op > quant|| op< 0){
+            printf("ERRO! Ingresso não encontrado!");
+            return 0;
+        }
+
+        printf("Digite o novo valor: \t");
+        scanf("%f", &valor);
+
+        altera_preco(&vetor_i[op-1], valor);
+        break;
+    case 2: 
+        printf("Encerrando o programa..");
+        break;
+    default:
+        printf("Opçao inválida!");
+        break;
+    }
+
+    for (cont = 0; cont < quant; cont++){
+        imprime(&vetor_i[cont], cont);
+    }
+    
+    imprime_maior_e_menor(quant, vetor_i);
+
+    return 0;
+}
