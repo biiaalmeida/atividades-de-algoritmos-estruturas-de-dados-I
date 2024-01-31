0c1e32c56537dc8d67957e96277bc3fec9ea2cc7
Ana Beatriz Almeida <112524121+biiaalmeida@users.noreply.github.com>
2024-01-31 17:13:59 -0300

    Create questao3.c

---
+++ unidade2/1.arquivos/listas/lista2/questao3.c
@@ -0,0 +1,16 @@
+/*Vocˆe foi contratado para desenvolver um programa em C que permita o cadastro de frutas e seus
+pre¸cos em um arquivo de dados. O programa deve usar uma estrutura (struct) para armazenar os
+detalhes de cada fruta, incluindo o nome da fruta e o pre¸co.
+O programa deve realizar as seguintes a¸c˜oes:
+Definir uma estrutura chamada Fruta com os seguintes campos:
+– nome (string) para armazenar o nome da fruta.
+– preco (float) para armazenar o pre¸co da fruta.
+Permitir que o usu´ario insira os dados das frutas via teclado.
+O usu´ario poder´a cadastrar m´ultiplas frutas em uma ´unica execu¸c˜ao do programa. Para cada fruta,
+o programa deve solicitar:
+O nome da fruta.
+O pre¸co da fruta.
+Ap´os inserir os dados de cada fruta, o programa deve salvar as informa¸c˜oes no arquivo ”frutas.txt”.
+Cada linha do arquivo deve conter o nome da fruta e seu pre¸co, separados por v´ırgula.
+O programa deve continuar solicitando os dados das frutas at´e que o usu´ario decida parar.
+Quando o usu´ario decidir parar de cadastrar frutas, o programa deve exibir uma mensagem de encerramento e fechar o arquivo.*/
