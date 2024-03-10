01fc1fd1e95add9c25ed47e5dd5f2c20b3ef994c
biiaalmeida <abeatrizpb@gmail.com>
2024-03-10 11:39:46 -0300

    delete

--- unidade2/1.arquivos/listas/listas1/questao2/questao2.c
+++
@@ -1,3 +0,0 @@
-/*Escreva um programa em C que preencha um vetor de 10 inteiros com informações
-vindas de um arquivo e escreva em outro arquivo o menor elemento, o maior elemento, bem
-como a média dos elementos do vetor, como ilustrado a seguir. */
